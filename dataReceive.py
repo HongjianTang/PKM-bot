2670d1f7fc0b52066e321824316cde555e060307
HongjianTang <wanquanbuhuiyingwen@gmail.com>
2023-11-08 11:32:32 +0000

    save as json

--- dataReceive.py
+++ dataReceive.py
@@ -1,4 +1,5 @@
 from replit import db
+import json
 
 
 class DataReceiver:
@@ -21,9 +22,13 @@ class DataReceiver:
 
     # Append the received data to the messages list
     self.messages.append(data_string)
+    # create a json object, "text" is data_string, "date" is 2023-10-01, "time" is 10:00
+    newJson = {"text": data_string, "date": "2023-11-08", "time": "10:00"}
+
     # insert message to database, the key is equal to the number of keys in database
-    db[str(len(db.keys()))] = data_string
+    db[str(len(db.keys()))] = json.dumps(newJson)
 
     # Print the current list of messages
     for key in db.keys():
-      print(key+": "+db[key])
+      data = json.loads(db[key])
+      print(key + ": " + data["text"])
